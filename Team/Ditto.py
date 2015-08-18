127bb1a6c5356cb057bc78e7688a8e20ff02812e
jedislight <jedislight@gmail.com>
2015-08-17 20:36:46 -0500

    Making Ditto safe to fight itself

--- Team/Ditto.py
+++ Team/Ditto.py
@@ -6,10 +6,17 @@ from AI import AI
 @AI.Team
 class Ditto(AI):
     def __init__(self):
-        self.other = [team for team in AI.team if team != Ditto][0]()
+        try:
+            self.other = [team for team in AI.team if team != self.__class__][0]()
+        except:
+            self.other = None
     
     def Update(self, ai_input):
-        return self.other.Update(ai_input)
+        if self.other:
+            return self.other.Update(ai_input)
+        return AI.Update(self, ai_input)
     
     def GetPlayerStats(self, player_number):
-        return self.other.GetPlayerStats(player_number)
+        if self.other:
+            return self.other.GetPlayerStats(player_number)
+        return AI.GetPlayerStats(self, player_number)
